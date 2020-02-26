6efdae0f1d522108f6c63c783aefdf2d0e852964
Urs Voegele <unext@bluewin.ch>
2020-02-26 14:40:23 +0100

    initial dnsmasq

--- provisioning/ubuntu-18.04-dnsmasq.sh
+++ provisioning/ubuntu-18.04-dnsmasq.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
 
-# define shared folders
-SHAREDFOLDERDIR="/mnt/sharedfolder"
+# set color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
-# update ubuntu
-sudo apt -y update
+echo -e "${BLUE}##################################${NC}"
+echo -e "${BLUE}# >>>> start provisioning         ${NC}"
+echo -e "${BLUE}##################################${NC}"
 
-# install nfs kernel
-sudo apt install -y nfs-kernel-server
-sudo apt install -y lsb-core
+# install dnsmasq
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> install dnsmasq           ${NC}"
+echo -e "${GREEN}#################################${NC}"
+sudo apt-get install -y dnsmasq
+sudo cp /vagrant/files/dnsmasq/dnsmasq.conf /etc/dnsmasq.conf
 
-# create shared folder
-sudo mkdir -p $SHAREDFOLDERDIR
-sudo chown nobody:nogroup $SHAREDFOLDERDIR
-sudo chmod 777 $SHAREDFOLDERDIR
+# disable systemd-resolved
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> disable systemd-resolved  ${NC}"
+echo -e "${GREEN}#################################${NC}"
+RESOLVFILE="/etc/resolv.conf"
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo rm $RESOLVFILE
 
-# add shared folder to exports
-echo "# $SHAREDFOLDERDIR  172.16.1.0/255.255.255.0(rw,async)" >> /etc/exports
+# create resolve.conf
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> create resolve.conf       ${NC}"
+echo -e "${GREEN}#################################${NC}"
+sudo cp  /vagrant/files/dnsmasq/resolv.conf $RESOLVFILE
+sudo chattr +i $RESOLVFILE
+sudo lsattr $RESOLVFILE
 
-# create date string
+# test syntax resolve.conf
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> test resolve.conf ${NC}"
+echo -e "${GREEN}#################################${NC}"
+sudo dnsmasq --test
+
+# restart dnsmasq
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> restart dnsmasq ${NC}"
+echo -e "${GREEN}#################################${NC}"
+sudo systemctl restart dnsmasq
+
+# get timestamp
+echo -e "${GREEN}#################################${NC}"
+echo -e "${GREEN}# >>>> restart dnsmasq ${NC}"
+echo -e "${GREEN}#################################${NC}"
 DATE=`date +%Y%m%d%H%M`
 
 # set version
-UBUNTU_VERSION=$(lsb_release -a | grep Release | awk  '{print $2}')
-echo "ubuntu-$UBUNTU_VERSION" > /vagrant/version
+DNSMASQ_VERSION=$(dnsmasq --version | awk  '{print $3}')
+echo "$DNSMASQ_VERSION" > /vagrant/version
+
+echo -e "${BLUE}#################################${NC}"
+echo -e "${BLUE}# >>>> end provisioning          ${NC}"
+echo -e "${BLUE}#################################${NC}"
